4cd21cc601147f33a64f63f71112a0a1b141eeba
Jase Kraft <jase@perfectmypodcast.com>
2021-09-14 21:48:57 -0500

    html & css for Login in page

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -4,4 +4,12 @@ from flask import render_template, redirect, request
 
 @app.route('/')
 def mainPage():
-    return render_template('main.html')
+    return render_template('login.html')
+
+@app.route('/register')
+def registerNewUser():
+    return "New user should be added"
+
+@app.route('/login')
+def loginUser():
+    return "User should go to user dashboard"
